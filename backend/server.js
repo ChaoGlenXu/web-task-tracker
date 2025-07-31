4396f61486455e6353d6ebb7cd0332d5cff5ac05
Chao (Glen) Xu <6476385788xc@gmail.com>
2025-07-31 05:36:52 -0700

    try

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ app.use("/api/tasks", taskRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html")); //"frontend", "dist", "index.html"
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")); //"frontend", "dist", "index.html"
     });
 };
 
