022a6dfd68d732d75aadc8d319de8778bf68d016
Chao (Glen) Xu <6476385788xc@gmail.com>
2025-07-31 04:38:44 -0700

    trying to fix deploy

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,11 @@ import taskRoutes from "./routes/task.route.js";
 import cors from "cors"; // this code may not be needed since deploying monolithic 
 import path from "path";
 
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -25,7 +30,7 @@ const _dirname = path.resolve();
 if (process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html")); //"frontend", "dist", "index.html"
     });
 };
 
